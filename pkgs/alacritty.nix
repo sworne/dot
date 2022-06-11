aa997dd401434de88afbae8b69c63fe31d47dabf
Samuel Worne <samuel@worne.xyz>
2022-06-11 17:34:59 +1000

    update desktop role

--- pkgs/alacritty.nix
+++ pkgs/alacritty.nix
@@ -3,5 +3,6 @@
   alacritty-conf = utils.mustache "alacritty-conf" ../dotfiles/alacritty/alacritty.yml.mustache theme;
 in
 {
+    programs.alacritty.enable = true;
     xdg.configFile."alacritty/alacritty.yml".source = alacritty-conf;
 }
---
+++ roles/desktop.nix
@@ -0,0 +1,14 @@
+{ ... }: {
+  imports = [
+    ./dwm.nix
+    ../pkgs/st.nix
+    ../pkgs/xsecurelock.nix
+    ../pkgs/vpn.nix
+    ../pkgs/spotify.nix
+    ../pkgs/media.nix
+    ../pkgs/photos.nix
+    ../pkgs/games.nix
+    ../pkgs/gtk.nix
+    ../pkgs/default.nix
+  ];
+}
