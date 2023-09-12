9218016086c0637bf5e8b0774b6da8df5cb64b2e
Samuel Worne <samuel@worne.xyz>
2023-09-12 11:54:49 +1000

    home manager is buggy

--- roles/default.nix
+++ roles/default.nix
@@ -6,7 +6,7 @@
 
   nixpkgs.config.allowUnfree = true;
   home.stateVersion = "23.05";
-  home.manual.manpages.enable = false;
+  manual.manpages.enable = false;
   _module.args = {
     utils = import ../utils { inherit pkgs; };
     theme = themes.theme.navy;
